0a4b9c763fa49956f0f47c122a3b675e61857aa5
judy-chelangat <judysigilai75@gmail.com>
2023-07-13 23:51:32 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// function findMatching(drivers,name){
+
+// return drivers.filter(function(driver){
+//     driver.toLowerCase()===name.toLowerCase()
+   
+// })
+// }
+// function fuzzyMatch(names,str){
+
+// }
+// function matchName(obj,str){
+
+// }
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+//✔ returns all drivers that match the passed in name
+const findMatching = (p,q) => p.filter(p => p.toLowerCase() === q.toLowerCase())
+//✔ returns a driver if beginning provided letters match
+const fuzzyMatch = (p,q) => p.filter(p => p.charAt(0) === q.charAt(0))
+//✔ accesses the data structure to check if name matche
+const matchName = (p,q) => p.filter(p => p.name === q)
